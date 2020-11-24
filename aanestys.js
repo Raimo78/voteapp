717ba74c3ca20a11430111f184a233a40b434deb
Raimo78 <62938861+Raimo78@users.noreply.github.com>
2020-11-24 08:49:10 +0200

    aanestys.js
    This file has been tested.

---
+++ aanestys.js
@@ -0,0 +1,24 @@
+function clicksubmit(){
+
+    var votehdokas1 = document.getElementById('ehdokas1'); 
+    alert("You voted for "+ voteehdokas1);
+    var votetrump = document.getElementById('ehdokas2'); 
+    alert("You voted for "+ voteehdokas2);
+    var votehillary = document.getElementById('ehdokas3'); 
+    alert("You voted for "+ voteehdokas3);
+    var votetrump = document.getElementById('ehdokas4'); 
+    alert("You voted for "+ voteehdokas4);
+    var votehillary = document.getElementById('ehdokas5'); 
+    alert("You voted for "+ voteehdokas4);
+    var votetrump = document.getElementById('ehdokas6'); 
+    alert("You voted for "+ voteehdokas5);
+    var votehillary = document.getElementById('ehdokas7'); 
+    alert("You voted for "+ voteehdokas6);
+    var votetrump = document.getElementById('ehdokas8'); 
+    alert("You voted for "+ voteehdokas7);
+    var votehillary = document.getElementById('ehdokas9'); 
+    alert("You voted for "+ voteehdokas8);
+    var votetrump = document.getElementById('ehdokas10'); 
+    alert("You voted for "+ voteehdokas9);
+
+}
